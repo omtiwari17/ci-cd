f2358fa2f4c89d6c905af8056c4484c187dfea75
Om Tiwari <work.om.tiwari@gmail.com>
2025-12-08 00:55:57 +0530

    Update assignment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "3rem" }}>
-      <h1>CI/CD + Docker Demo</h1>
+      <h1>CI/CD + Docker Assignment</h1>
       <p>Branch: dev → Docker Hub → Render → Done</p>
     </div>
   );
